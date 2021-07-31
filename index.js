f3f77c614fbec6dc2fd6df97f6866de6955e6065
catupperman <catupperman@gmail.com>
2021-07-31 17:23:57 -0400

    editing up license adding extra question sections

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function getLicenseBadge(license) {
         return `![MIT](https://img.shields.io/badge/License-MIT-blue)`
     } else if (license === "Appache 2.0"){
         return `![Appache 2.0](https://img.shields.io/badge/License-Appache%202.0-blue)`
+    } else if (license === "Appache 2.0"){
+        return `![BSD](https://img.shields.io/badge/License-BSD-blue)`
+    }  else {
+        return `![BSD](https://img.shields.io/badge/License-GPL-blue)`
     }
 }
 
@@ -29,26 +33,66 @@ function getLicenseInfo(license) {
         
         THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."`
     } else if (license === "Appache 2.0"){
-        return `"Appache is the best"`
+        return `"Copyright 2021 catupperman
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+        
+            http://www.apache.org/licenses/LICENSE-2.0
+        
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License."`
+    } else if (license === "BSD"){
+        return `"Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+        
+        2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+        
+        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+        
+        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."`
+    } else {
+        return `"This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+    
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+    
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>."`
     }
 }
 
 function generateMD(responses) {
     return `
 # ${responses.title}
-${getLicenseBadge(responses.license)}
 ## Table of Contents
 * [installation](#installation)
 * [description](#description)
 * [methods](#methods)
 * [contributions](#contributions)
 * [testing](#testing)
-* [license](#license)
+* [license](#license) 
 
 
 ## Installation
 > ${responses.installation}
+## Description of Project
+## Methods Used
+## Contributions
+## Testing Methods
 ## License
+${getLicenseBadge(responses.license)}
+Copyright (C) 2021 ${responses.username}
 ${getLicenseInfo(responses.license)}
             `
 
@@ -91,6 +135,21 @@ inquirer
             message: "What license are you using?",
             name: "license",
             choices: ["MIT", "Appache 2.0", "BSD", "GPL"]
+        },
+        {
+            type: "input",
+            message: "What is your GitHub user name?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What's your email address?",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "What is your preferred method of contact?",
+            name: ["Email", "Text", "Owl"]
         }
     ])
     .then((answers) => {
